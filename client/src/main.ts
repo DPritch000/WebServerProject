cf78dd95f02ed530899588132ce8f3e97a63ab65
Deven Pritchard <pritchad1@newpaltz.edu>
2026-05-14 13:00:40 -0400

    Ai generated Data

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,13 +2,22 @@ import './assets/main.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import Oruga from '@oruga-ui/oruga-next'
+import '@oruga-ui/oruga-next/dist/oruga.css' // Oruga styles
+// optional Bulma
+import 'bulma/css/bulma.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+const orugaConfig = {
+	// example global config: use FontAwesome icons
+	iconPack: 'fas'
+}
+
+//app.use(Oruga, orugaConfig)
 
 app.mount('#app')
